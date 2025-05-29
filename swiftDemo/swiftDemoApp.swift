a069bc244f34fd4a3a9d2ee0650c25a03c28f936
vooxlinc000 <linc@voox.com>
2025-05-29 19:46:37 +0800

    add demo
    demo

---
+++ swiftDemo/swiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDemoApp.swift
+//  swiftDemo
+//
+//  Created by y on 2024/6/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
